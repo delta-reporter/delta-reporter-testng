7daca39c09b328bfa3dc5639fd2217d747c57090
Alex Pishcheiko <oleksandra.pishcheiko@distilled.ie>
2020-10-14 10:20:35 +0100

    adding two more suites for testing

---
+++ src/test/java/com/deltareporter/AppTest3.java
@@ -0,0 +1,20 @@
+package com.deltareporter;
+
+import static org.testng.Assert.assertTrue;
+
+import org.testng.annotations.Test;
+
+/** Unit test for simple App. */
+public class AppTest3 {
+  /** Rigorous Test :-) */
+
+  @Test
+  public void shouldAnswerWithFalse123() {
+    assertTrue(false);
+  }
+
+  @Test
+  public void shouldAnswerWithFalse144() {
+    assertTrue(false);
+  }
+}
---
+++ src/test/java/com/deltareporter/AppTest4.java
@@ -0,0 +1,20 @@
+package com.deltareporter;
+
+import static org.testng.Assert.assertTrue;
+
+import org.testng.annotations.Test;
+
+/** Unit test for simple App. */
+public class AppTest4 {
+  /** Rigorous Test :-) */
+
+  @Test
+  public void shouldAnswerWithTrue() {
+    assertTrue(true);
+  }
+
+  @Test
+  public void shouldAnswerWithTrue2() {
+    assertTrue(true);
+  }
+}
